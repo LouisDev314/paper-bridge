bf7ed599c7f9d9b1ce5647e6933d388120d844b6
Louis Chan <louiscch314@gmail.com>
2026-02-25 21:20:54 -0700

    config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,45 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      eqeqeq: ['error', 'always'],
+      'no-duplicate-imports': ['error', { includeExports: true }],
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      'no-unused-expressions': 'off',
+      complexity: ['warn', 20],
+
+      indent: 'off',
+      quotes: 'off',
+      'no-multi-spaces': 'off',
+      'linebreak-style': 'off',
+      'object-curly-spacing': 'off',
+      'eol-last': 'off',
+      'comma-dangle': 'off',
+      'nonblock-statement-body-position': 'off',
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'components/ui/**',
   ]),
 ]);
 
